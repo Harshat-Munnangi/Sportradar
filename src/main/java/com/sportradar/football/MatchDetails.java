597ccb99b2801c6a138652fdec7c3ab9aea66ca3
Harshat-Munnangi <munnangi.harshat@gmail.com>
2022-07-03 15:46:51 +0200

    add pojo class

---
+++ src/main/java/com/sportradar/football/MatchDetails.java
@@ -0,0 +1,52 @@
+package com.sportradar.football;
+
+import java.time.LocalDateTime;
+
+public class MatchDetails {
+
+	private String homeTeam; 
+	private String awayTeam; 
+	private int homeTeamPoints; 
+	private int awayTeamPoints;
+	private LocalDateTime createdTime;
+	
+	public MatchDetails(String homeTeam, String awayTeam, int homeTeamPoints, int awayTeamPoints) {
+		super();
+		this.homeTeam = homeTeam;
+		this.awayTeam = awayTeam;
+		this.homeTeamPoints = homeTeamPoints;
+		this.awayTeamPoints = awayTeamPoints;
+		this.createdTime = LocalDateTime.now();
+	}
+	
+	public String getHomeTeam() {
+		return homeTeam;
+	}
+	public String getAwayTeam() {
+		return awayTeam;
+	}
+	public int getHomeTeamPoints() {
+		return homeTeamPoints;
+	}
+	public int getAwayTeamPoints() {
+		return awayTeamPoints;
+	}
+	public LocalDateTime getCreatedTime() {
+		return createdTime;
+	}
+	public void setHomeTeam(String homeTeam) {
+		this.homeTeam = homeTeam;
+	}
+	public void setAwayTeam(String awayTeam) {
+		this.awayTeam = awayTeam;
+	}
+	public void setHomeTeamPoints(int homeTeamPoints) {
+		this.homeTeamPoints = homeTeamPoints;
+	}
+	public void setAwayTeamPoints(int awayTeamPoints) {
+		this.awayTeamPoints = awayTeamPoints;
+	}
+	
+	
+	
+}
