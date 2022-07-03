9bfc00628570c1ef185360d0a88e5cd84290175d
Harshat-Munnangi <munnangi.harshat@gmail.com>
2022-07-03 15:58:44 +0200

    add switch cases

--- src/main/java/com/sportradar/football/FootballDashboardImpl.java
+++ src/main/java/com/sportradar/football/FootballDashboardImpl.java
@@ -2,6 +2,7 @@ package com.sportradar.football;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,7 +17,52 @@ public class FootballDashboardImpl implements FootballDashborad {
 		int optionSelected = 4;
 		Scanner in = new Scanner(System.in);
 		
+		while(optionSelected != 5) {
+			showMenu(options);
+			try {
+				optionSelected = in.nextInt();
+				switch (optionSelected) {
+				case 1 -> {
+					printOption(1);
+					break;
+				}
+				case 2 -> {
+					printOption(2);
+					break;
+				}
+				case 3 -> {
+					printOption(3);
+					break;
+				}
+				case 4 -> {
+					printOption(4);
+					break;
+				}
+				case 5 -> {
+					System.out.println("Application Closed!!");
+					System.exit(0);
+				}
+				default -> {
+					System.out.println("Please choose between 1 and " + options.size());
+				}
+				}
+			} catch (InputMismatchException ime) {
+				System.out.println("Please enter an integer value between 1 and " + options.size());
+				in.next();
+			}
+		}
 		in.close();
+	}
+	
+	
+	private void showMenu(List<String> options) {
+		System.out.println("--------------------------------------------------");
+		options.forEach(System.out::println);
+		System.out.println("Please select an option:");
+	}
+	
+	private void printOption(int i) {
+		System.out.println("Selected option "+i);
 	}
 
 }
